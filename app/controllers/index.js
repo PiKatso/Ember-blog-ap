406cc9941a0b1a1fe9e43a786ebf911c33349cbf
Aubrey Masten and Kat Gifford <kat@>
2017-06-01 15:45:56 -0700

    add css

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   segment: Ember.computed(function(){
-    return this.model.posts.store.peekAll('post').toArray().reverse().slice(0,5);
+    return this.model.posts.store.peekAll('post').toArray().reverse().slice(0,4);
   })
 });
